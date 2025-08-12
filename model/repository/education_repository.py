05b3c8af6189cd3e8bbaf27ead6bc6a39d5322ae
Student <parisamonfared97@gmail.com>
2025-08-12 17:10:10 +0330

    education_repository.py updated.

---
+++ model/repository/education_repository.py
@@ -0,0 +1,30 @@
+from database_manager import transaction_manager
+
+
+def save(person_id, university, grade, average, start_date, end_date):
+    transaction_manager('insert into education (person_id, university, grade, average, start_date, end_date) values (?,?,?,?,?,?)',
+                        [university, grade, average, start_date, end_date],
+                        commit=True)
+
+
+def edit(id, person_id, university, grade, average, start_date, end_date):
+    transaction_manager('update education set university=?,'
+                        ' grade=?, average=?, start_date=?, end_date=? where id=?',
+                        [person_id,university, grade, average, start_date, end_date,id],
+                        commit=True)
+
+
+def remove(id):
+    transaction_manager('delete from education where id =?',
+                        [id],
+                        commit=True)
+
+def find_all():
+    education_list = transaction_manager(
+        'select * from education')
+    return education_list
+
+
+def find_by_id(id):
+    education = transaction_manager('select * from education where id = ?',[id])
+    return education
