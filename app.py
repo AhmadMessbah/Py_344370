eb396f2a9c11c96a2019a6b8b81eb92249a97d34
Student <ANOOSH114466@GMAIL.COM>
2025-08-12 17:36:05 +0330

    driver_licence_repository.py UPDATED

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from model.repository.database_manager import create_database
 create_database()
 print("Database Created")
 
-
 # Test
 import test.person_test
+
+from model.repository.driver_licence_repository import *
+save(1,"a1","b2","tehran",1,1,)
+
--- model/repository/driver_licence_repository.py
+++ model/repository/driver_licence_repository.py
@@ -1,23 +1,33 @@
 from model.repository.database_manager import *
 
 def save(person_id,serial,licence_type ,city,registered_date,expired_date):
-    transaction_manager(
-        "insert into driver_licence(person_id,serial,licence_type ,city,registered_date,expired_date) values (?,?,?,?,?,?)",
+    return transaction_manager(
+        "insert into driver_licences(person_id, serial, licence_type, city, registered_date, expired_date) values (?,?,?,?,?,?)",
         [person_id,serial,licence_type,city,registered_date,expired_date],
          commit = True
     )
 
 def edit(id,person_id,serial,licence_type,city,registered_date,expired_date):
-    transaction_manager(""
-                        "update driver_licence set id=?,person_id=?,serial=?,licence_type=?,city=?,registered_date=?,expired_date=?  ",
-                       [id,person_id,serial,licence_type,city,registered_date,expired_date],
+    return transaction_manager(""
+                        "update driver_licences set id=?,person_id=?,serial=?,licence_type=?,city=?,registered_date=?,expired_date=?  where id=?",
+                       [person_id,serial,licence_type,city,registered_date,expired_date, id],
          commit = True
    )
 
 def remove(id):
-    transaction_manager("delete from driver_licence where id=?",[id],commit = True)
+    return transaction_manager("delete from driver_licences where id=?",[id],commit = True)
 
 
 
+def find_all():
+    license_list = transaction_manager("select * from driver_licences")
+    return license_list
 
+def find_by_id(id):
+    person_id = transaction_manager("select * from driver_licences where id=?",[id])
+    return person_id
+
+def find_by_serial(serial):
+    license_serial= transaction_manager("select * from driver_licences where serial=?",[serial])
+    return license_serial
 
