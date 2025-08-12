afe09202725b4fe2812d46ad8b66ddda01658907
Messbah.A <Messbah.A@Gmail.com>
2025-08-12 17:18:20 +0330

    salary updated

--- model/repository/salary_repository.py
+++ model/repository/salary_repository.py
@@ -5,7 +5,7 @@ from model.repository.database_manager import transaction_manager
 
 def save(person_id, weekly_hours, pay_for_hours, end_date, employment_type):
     return transaction_manager(
-        "insert into salary(person_id, weekly_hours, pay_for_hours, end_date, employment_type) values (?,?,?,?,?)",
+        "insert into salaries(person_id, weekly_hours, pay_for_hours, end_date, employment_type) values (?,?,?,?,?)",
         [person_id, weekly_hours, pay_for_hours, end_date, employment_type],
         commit=True
     )
@@ -13,23 +13,23 @@ def save(person_id, weekly_hours, pay_for_hours, end_date, employment_type):
 
 def edited():
     return transaction_manager(
-        "update salary set weekly_hours = ? ,pay_for_hours=? where person_id = ?",
+        "update salaries set weekly_hours = ? ,pay_for_hours=? where person_id = ?",
     )
 
 
 def delete():
     return transaction_manager(
-        "delete from salary where id = ?",
+        "delete from salaries where id = ?",
     )
 
 
 def find_all():
     return transaction_manager(
-        "select * from salary",
+        "select * from salaries",
     )
 
 
 def find_by_id():
     return transaction_manager(
-        "select person_id from salary ",
+        "select person_id from salaries ",
     )
