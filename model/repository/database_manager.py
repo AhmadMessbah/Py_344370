7b5098cf4b1e2b00dedb1e563047d671cc0fd642
Student <ANOOSH114466@GMAIL.COM>
2025-08-12 16:37:29 +0330

    DRIVER LICENSE TABLE CREATED

--- model/repository/database_manager.py
+++ model/repository/database_manager.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 def transaction_manager(sql_command, parameter_list, commit=False):
     connection = sqlite3.connect('./model/repository/class_project.db')
     cursor = connection.cursor()
@@ -26,7 +27,20 @@ def create_database():
         """
     )
 
-
+    # DRIVER LICENSE table
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS driver_licence(
+             id INTEGER PRIMARY KEY AUTOINCREMENT,
+             person_id REFERENCES PERSONS,
+             serial TEXT,
+             licence_type TEXT,
+             city TEXT,
+             registered_date TEXT,
+             expired_date TEXT
+        )
+        """
+    )
 
     cursor.close()
     connection.close()
