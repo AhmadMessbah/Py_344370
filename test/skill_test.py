0b664dac598135954b408590fcd479f46e28c4d0
artemisslotfi <artemiss8686@gmail.com>
2025-08-16 14:24:43 -0700

    skill updated

--- test/skill_test.py
+++ test/skill_test.py
@@ -1,2 +1,22 @@
 from model.repository.skill_repository import *
 
+# test passed
+# save(102,"project managemant","Global Enterprises","3 years","2022-08-20",15)
+
+# test passed
+# edit(2,101,"graphic design","Creative Studio","1 years","2022-05-20",15)
+
+# test passed
+# remove(2)
+
+# test passed
+# print(find_all())
+
+# test passed
+# print(find_by_id(1))
+
+# test passed
+# print(find_by_title_and_institute("g","Creative"))
+
+
+# skill_repository test passed
