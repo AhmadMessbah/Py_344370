50d6327d9288094464f25b9d5b855168e3f766c9
Student <parisamonfared97@gmail.com>
2025-08-17 17:10:13 +0330

    education_repository.py updated

--- model/repository/education_repository.py
+++ model/repository/education_repository.py
@@ -2,33 +2,33 @@ from model.repository.database_manager import transaction_manager
 
 
 def save(person_id, university, grade, average, start_date, end_date):
-    transaction_manager('insert into education (person_id ,university, grade, average, start_date, end_date) values (?,?,?,?,?,?)',
+    transaction_manager('insert into educations (person_id ,university, grade, average, start_date, end_date) values (?,?,?,?,?,?)',
                         [person_id, university, grade, average, start_date, end_date],
                         commit=True)
 
 
 def edit(id, person_id, university, grade, average, start_date, end_date):
-    transaction_manager('update education set person_id=?, university=?,'
+    transaction_manager('update educations set person_id=?, university=?,'
                         ' grade=?, average=?, start_date=?, end_date=? where id=?',
                         [person_id, university, grade, average, start_date, end_date,id],
                         commit=True)
 
 
 def remove(id):
-    transaction_manager('delete from education where id =?',
+    transaction_manager('delete from educations where id =?',
                         [id],
                         commit=True)
 
 def find_all():
     education_list = transaction_manager(
-        'select * from education')
+        'select * from educations')
     return education_list
 
 
 def find_by_id(id):
-    education = transaction_manager('select * from education where id = ?',[id])
+    education = transaction_manager('select * from educations where id = ?',[id])
     return education
 
 def find_by_person_id(person_id):
-    person = transaction_manager('select * from education where person_id = ?',[person_id])
+    person = transaction_manager('select * from educations where person_id = ?',[person_id])
     return person
