b24531ab31e5d55b43e2de088151c637aadc0ab3
Student <parisamonfared97@gmail.com>
2025-08-17 17:11:47 +0330

    education updated

--- model/service/education_service.py
+++ model/service/education_service.py
@@ -1,21 +1,32 @@
 from model.repository import education_repository
 
-def save(person_id, university, grade, average, start_date, end_date):
-    if 15>=average :
-        raise ValueError("معدل زیر 15 مناسب نیست")
-    return education_repository.save(person_id, university, grade, start_date, end_date)
 
-def edit(id, university, grade, average, start_date, end_date):
-    student= education_repository.find_by_id(id)
-    if student :
+def save(person_id, university, grade, average, start_date, end_date):
+    if 15 >= average:
+        raise ValueError("معدل زیر 15 مناسب نیست")
+    return education_repository.save(person_id, university, grade, average, start_date, end_date)
+
+
+def edit(id, person_id, university, grade, average, start_date, end_date):
+    student = education_repository.find_by_id(id)
+    if student:
         if 15 >= average:
             raise ValueError("معدل زیر 15 مناسب نیست")
-        return education_repository.edit(id, university, grade, start_date, end_date)
+        return education_repository.edit(id, person_id, university, grade, average, start_date, end_date)
+
+
 def delete(id):
-    student= education_repository.find_by_id(id)
-    if student :
+    student = education_repository.find_by_id(id)
+    if student:
         return
+
+
 def find_all():
     pass
+
+
 def find_by_id(id):
+    pass
+
+def find_by_person_id(person_id):
     pass
