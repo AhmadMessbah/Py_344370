912444b10f522334f2fd15df9cea0b1c7f8b7e16
Student <soroush.hosseini.1@gmail.com>
2025-08-17 17:15:21 +0330

    job updated

---
+++ controller/job_history_controller.py
@@ -0,0 +1,22 @@
+import re
+
+from model.service import job_history_service
+
+
+def save():
+    pass
+
+def edit():
+    pass
+
+def delete(id):
+    pass
+
+def find_all():
+    pass
+
+def find_by_id(id):
+    pass
+
+def find_by_job_title(job_title):
+    pass
---
+++ model/service/job_history_service.py
@@ -0,0 +1,19 @@
+from model.repository import job_history_repository
+
+def save():
+    pass
+
+def edit():
+    pass
+
+def delete(id):
+    pass
+
+def find_all():
+    pass
+
+def find_by_id(id):
+    pass
+
+def find_by_job_title(job_title):
+    pass
