02342b2730e51d5ada96184b6ae7a1e45f37df3d
parisa_monfared <parisamonfared97@gmail.com>
2025-08-19 00:25:10 +0330

    education_service.py edited and test passed successfully.

--- test/education_test.py
+++ test/education_test.py
@@ -1,16 +1,38 @@
-from model.repository.education_repository import *
+# from model.repository.education_repository import *
+from model.service import education_service
 
-#test passed
-# save('2525005','azad ',"bachelor",20,"2017","2021")
+# ---------------------------------------------------------------------------------
+# REPOSITORY
 
-#test passed
+# test passed
+# save('2525005','azad',"bachelor",20,"2017","2021")
+
+# test passed
 # edit(2,'',"elmo sanat university","bachelor",19,"2010","2014")
 
-#test passed
+# test passed
 # remove(1)
 
-#test passed
+# test passed
 # print(find_all())
 
-#test passed
+# test passed
 # print(find_by_person_id("2525005"))
+
+# ---------------------------------------------------------------------------------
+# SERVICE
+
+# test passed
+# education_service.save('1254125','elm o farhang',"bachelor",16,"2017","2021")
+
+# test passed
+# education_service.edit('','1254125','elm o farhang university',"bachelor",16,"2017","2021")
+
+# test passed
+# education_service.delete(4)
+
+# test passed
+# print(education_service.find_all())
+
+# test passed
+# print(education_service.find_by_person_id("1254125"))
