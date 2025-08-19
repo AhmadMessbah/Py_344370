6ed56195efde39e826853491047a3eb1a5775a99
Leila Farajnezhad <farajnezhad.leila@gmail.com>
2025-08-19 16:29:56 +0330

    create entity salary

---
+++ model/entity/salary.py
@@ -0,0 +1,15 @@
+class Salary:
+    def __init__(self,id,person_id,weekly_hour,pay_for_hours,end_date,employment_type):
+        self.id = id
+        self.person_id = person_id
+        self.weekly_hour = weekly_hour
+        self.pay_for_hours = pay_for_hours
+        self.end_date = end_date
+        self.employment_type = employment_type
+
+
+    def __repr__(self):
+        return f"{self.__dict__}"
+
+    def to_tuple(self):
+        return (self.id,self.person_id,self.weekly_hour,self.pay_for_hours,self.end_date,self.employment_type)
