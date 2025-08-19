f114b92e2936f710549ba2febe917cc4fe3ff81b
Leila Farajnezhad <farajnezhad.leila@gmail.com>
2025-08-19 16:37:18 +0330

    update  salary repository

--- model/repository/salary_repository.py
+++ model/repository/salary_repository.py
@@ -3,42 +3,44 @@ from linecache import updatecache
 from model.repository.database_manager import transaction_manager
 
 
-def save(person_id, weekly_hours, pay_for_hours, end_date, employment_type):
-    return transaction_manager(
-        "insert into salaries(person_id, weekly_hours, pay_for_hours, end_date, employment_type) values (?,?,?,?,?)",
-        [person_id, weekly_hours, pay_for_hours, end_date, employment_type],
-        commit=True
-    )
+class SalaryRepository:
+    def save(self, person_id, weekly_hours, pay_for_hours, end_date, employment_type):
+        return transaction_manager(
+            "insert into salaries(person_id, weekly_hours, pay_for_hours, end_date, employment_type) values (?,?,?,?,?)",
+            [person_id, weekly_hours, pay_for_hours, end_date, employment_type],
+            commit=True
+        )
 
+    def edit(self, id, person_id, weekly_hours, pay_for_hours, end_date, employment_type):
+        return transaction_manager(
+            "update salaries set weekly_hours = ? ,pay_for_hours=? where person_id = ?",
+            [person_id, weekly_hours, pay_for_hours, end_date, employment_type, id]
+        )
 
-def edit(id, person_id, weekly_hours, pay_for_hours, end_date, employment_type):
-    return transaction_manager(
-        "update salaries set weekly_hours = ? ,pay_for_hours=? where person_id = ?",
-        [person_id, weekly_hours, pay_for_hours, end_date, employment_type, id]
-    )
+    def delete(self,id):
+        return transaction_manager(
+            "delete from salaries where id = ?",
+            [id],
+            commit=True
+        )
 
+    def find_all(self):
+        return transaction_manager(
+            "select * from salaries",
+        )
 
-def delete(id):
-    return transaction_manager(
-        "delete from salaries where id = ?",
-        [id],
-        commit=True
-    )
+    def find_by_id(self,person_id):
+        return transaction_manager(
+            "select person_id from salaries ",
+        )
 
+    def find_by_person_id(self,person_id):
+        return transaction_manager(
+            "select * from salaries where person_id = ?",
+        )
 
-def find_all():
-    return transaction_manager(
-        "select * from salaries",
-    )
+    def find_by_employment_type(self,employment_type):
+        return transaction_manager(
+            "select * from salaries where employment_type = ?",
+        )
 
-
-def find_by_id():
-    return transaction_manager(
-        "select person_id from salaries ",
-    )
-
-def find_by_person_id(person_id):
-    pass
-
-def find_by_employment_type(employment_type):
-    pass
