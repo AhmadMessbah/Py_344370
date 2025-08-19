8e192d9355af72464ba30c08f46b3d94612a8324
Messbah.A <Messbah.A@Gmail.com>
2025-08-19 17:12:53 +0330

    person_fully completed

--- view/component/label_with_text.py
+++ view/component/label_with_text.py
@@ -3,5 +3,7 @@ from tkinter import Label, Entry
 
 class LabelWithText:
     def __init__(self, windows, text, variable, x, y):
-        Label(windows, text=text).place(x=x, y=y)
-        Entry(windows, textvariable=variable).place(x=x + 80, y=y)
+        self.label = Label(windows, text=text)
+        self.label.place(x=x, y=y)
+        self.text = Entry(windows, textvariable=variable,width=15)
+        self.text.place(x=x + 80, y=y)
