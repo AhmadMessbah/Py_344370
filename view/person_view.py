7f9f3878b7a4e386dce8ada451057f77521f2ee9
Messbah.A <Messbah.A@Gmail.com>
2025-08-19 16:14:47 +0330

    messbah oop added view added tested
    updated

--- view/person_view.py
+++ view/person_view.py
@@ -14,6 +14,7 @@ class PersonView:
     def __init__(self):
         self.person_controller = PersonController()
         self.win= Tk()
+        self.win.title("Person Profile")
         self.win.geometry("300x300")
 
         self.id = IntVar()
