152f26e59afe560670e0764140b22b461d63b9bb
Student <anoosh114466@gmail.com>
2025-08-19 17:27:16 +0330

    driver_licences_repository updated.

--- model/entity/driver_licence.py
+++ model/entity/driver_licence.py
@@ -1,4 +1,4 @@
-class DriverLicences:
+class DriverLicence:
     def __init__(self,id,person_id,serial,licence_type,city,registered_date,expired_date):
         self.id = id
         self.person_id = person_id
