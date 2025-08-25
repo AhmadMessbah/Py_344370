b560492af10d540f2b826a47740e422ca05a6bbb
Ahmad Mesbah <Messbah.A@Gmail.com>
2025-08-25 18:56:33 +0330

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,5 @@ print("App Started")
 from model.repository.database_manager import create_database
 create_database()
 print("Database Created")
-
 import test.person_test
-
-
-print("Salam")
+print("by")
