77f6b5964799b8a07a33cccc13319adcc6034643
Messbah.A <Messbah.A@Gmail.com>
2025-09-07 17:26:07 +0330

    orm framework added

--- app.py
+++ app.py
@@ -3,17 +3,8 @@ from model.tools.logging import Logger
 
 Logger.info("App Started")
 
-# import test.person_test
-# from view.person_view import PersonView
-# ui = PersonView()
+import test.person_test
 
 
-
-person1 = Person("A", "B", 10)
-person2 = Person("A", "B", 10)
-person3 = Person("A", "B", 10)
-
-
-person_list = [person1, person2, person3]
-print(person_list)
-
+from view.person_view import PersonView
+ui = PersonView()
