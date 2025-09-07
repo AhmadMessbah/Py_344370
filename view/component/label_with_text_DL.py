e09a31df08e80773c4e4bc29356fb1272aee89d8
Messbah.A <Messbah.A@Gmail.com>
2025-09-07 17:32:52 +0330

    orm framework added

--- view/component/label_with_text_DL.py
+++
@@ -1,7 +0,0 @@
-from view.component.label_with_text import *
-class LabelWithTextDriverLicence:
-    def __init__(self, windows, text, variable, x, y, state="normal"):
-        self.label = Label(windows, text=text)
-        self.label.place(x=x, y=y)
-        self.text = Entry(windows, textvariable=variable, width=15, state=state)
-        self.text.place(x=x + 100, y=y)
